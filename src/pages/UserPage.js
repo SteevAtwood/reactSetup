4e40a8d673d86aad8980bfe8072d24661db9adfd
Yozhef <satwoodt@gmail.com>
2023-05-11 21:53:54 +0200

    updated

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,13 +1,10 @@
 import React from "react";
-import WrappedPattern from "../components/WrappedPattern/WrappedPattern";
 
 const UserPage = () => {
   return (
-    <WrappedPattern>
-      <div>
-        <h1>Welcome User</h1>
-      </div>
-    </WrappedPattern>
+    <div> 
+      <h1>Welcome User</h1>
+    </div>
   );
 };
 
