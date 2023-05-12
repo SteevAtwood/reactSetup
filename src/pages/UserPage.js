389d17639ab06b4350a7de0f55353a0f5910fdca
Yozhef <satwoodt@gmail.com>
2023-05-12 16:42:05 +0200

    just styling for myself

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-import "./UserPage.css"
+import "./UserPage.css";
 
 const UserPage = () => {
   return (
-    <div> 
-      <h1 className="main-h1">Welcome User</h1>
+    <div>
+      <form className="main-content">
+        <h1 className="main-h1">Welcome User</h1>
+        <label>
+          Write Password:
+          <input className="main-input-search" type="password" name="password" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
