ec8f29b9e1bc27fab4818f3d7c30a03f6f98dc06
Yozhef <satwoodt@gmail.com>
2023-05-16 10:40:31 +0200

    fetching data

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,57 +1,67 @@
 import React, { useEffect, useState } from "react";
 import { useKeycloak } from "@react-keycloak/web";
 import { Settings } from "../Settings";
+import Wrapped from "../components/Wrapped/Wrapped";
+import { Navigate } from "react-router-dom";
 
 import "./UserPage.css";
 
 const UserPage = () => {
   const { keycloak } = useKeycloak();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [inputValue, setInputValue] = useState("");
 
   const searchBarHandler = (event) => {
     event.preventDefault();
-    fetch(Settings.BACKEND_URL + Settings.BACKEND_ENDPOINTS.SEARCH, {
+    fetch(`http://localhost:8081/search/${inputValue}`, {
       method: "get",
       headers: {
         Accept: "application/json",
-        Authorization: "Bearer " + keycloak.token,
       },
     })
-      .then((response) => {
-        console.log(response);
-        return response.json();
-      })
+      .then((response) => response.json())
+      .then((receivedData) => {
+        console.log(receivedData);
+        setData(receivedData);
+      });
+  };
 
-      .then((recievedData) => {
-        console.log(recievedData);
-        setData(recievedData);
-      })
-      .catch(console.error);
+  const inputValueHandler = (event) => {
+    setInputValue(event.target.value);
   };
 
   return (
     <>
       <div>
-        <form onSubmit={searchBarHandler} className="main-content">
+        <form onSubmit={searchBarHandler}>
           <h1 className="main-h1">Welcome User</h1>
-          <label>
-            Write Password:
+          <h2 className="main-h2">Please enter a password to get data...</h2>
+          <label className="main-label">
             <input
               className="main-input-search"
               type="password"
               name="password"
+              value={inputValue}
+              onChange={inputValueHandler}
             />
           </label>
-          <input type="submit" value="Submit" />
+          <button type="submit" className="main-btn">Submit</button>
         </form>
       </div>
       <div>
-        {data.map((item) => (
-          <div key={item.id}>
-            <h2>{item.text}</h2>
-            <p>{item.link}</p>
-          </div>
-        ))}
+        <p>
+          {data ? (
+            <div>
+              <p>Here is your wanted book...</p>
+              <ul>
+                <li>{data.text}</li>
+                <li>{data.link}</li>
+              </ul>
+            </div>
+          ) : (
+            "Nothing to read yet..."
+          )}
+        </p>
       </div>
     </>
   );
